1eaaab0b040374e9bae9b3ae31a9cb240c19be21
urclown <yourclown31@gmail.com>
2025-01-06 12:27:30 +0800

    Add files via upload

---
+++ gabut2/script.js
@@ -0,0 +1,6 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    document.getElementById('hiddenMessage').classList.remove('hidden');
+});
+document.getElementById('acceptButton').addEventListener('click', function() {
+    alert('Kirimmm pappp dongg banyakinn kaloo kangenn.');
+});
